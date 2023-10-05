437a3f397a0a3c25143d5aeafd734685a0b75f4c
Ca4eWoo <thecvcoder@foxmail.com>
2023-10-05 07:12:08 +0000

    modify file [service.user_service.go] RegisterByEmail

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -1,12 +1,23 @@
 package service
 
-import "github.com/3lur/go-mall/internal/repo"
+import (
+	"context"
+
+	"github.com/3lur/go-mall/internal/repo"
+	"github.com/3lur/go-mall/internal/schema"
+)
 
 type UserService interface {
+	RegisterByEmail(ctx context.Context, schema *schema.UserRegisterRequest) error
 }
 
 type userService struct {
 	userRepo repo.UserRepo
+}
+
+// RegisterByEmail implements UserService.
+func (*userService) RegisterByEmail(ctx context.Context, schema *schema.UserRegisterRequest) error {
+	panic("unimplemented")
 }
 
 func NewUserService(userRepo repo.UserRepo) UserService {
