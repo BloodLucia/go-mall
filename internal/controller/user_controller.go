177e72889fbda45f3c0bdffc025e6fd1211cc06d
Ca4eWoo <thecvcoder@foxmail.com>
2023-10-05 07:03:34 +0000

    modify file [controller.user_controller.go] Register

--- internal/controller/user_controller.go
+++ internal/controller/user_controller.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"net/http"
+
+	"github.com/3lur/go-mall/internal/schema"
 	"github.com/3lur/go-mall/internal/service"
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +14,12 @@ type userController struct {
 
 // Register implements UserController.
 func (*userController) Register(ctx *gin.Context) {
+	var req schema.UserRegisterRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"error": err,
+		})
+	}
 	ctx.String(200, "user register")
 }
 
